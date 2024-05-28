0937aa345afa68e4f644e4fd6f01e54400100beb
David Isinta <david.isinta@gmail.com>
2024-05-28 16:26:08 +0300

    made basic get request

--- src/main/java/com/institution_manager/institution_manager_app/InstitutionController.java
+++ src/main/java/com/institution_manager/institution_manager_app/InstitutionController.java
@@ -1,5 +1,6 @@
 package com.institution_manager.institution_manager_app;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,6 +8,11 @@ public class InstitutionController
 {
 
     //add a new institution - create(POST)
+    @RequestMapping("/home")
+    public String createInstitution()
+    {
+        return "Heyyy theree, you have created an instituiton named Yale uni! yaaahhh!! lets get it";
+    }
 
     // get all institutions - read(GET)
 
