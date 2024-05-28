171d88d6b5951eed62fa63fa634b7f5c5e86894d
David Isinta <david.isinta@gmail.com>
2024-05-28 15:46:57 +0300

    first commit

---
+++ src/main/java/com/institution_manager/institution_manager_app/InstitutionManagerAppApplication.java
@@ -0,0 +1,16 @@
+package com.institution_manager.institution_manager_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutionManagerAppApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("heyyy thereee!! salaam brother!!");
+
+		SpringApplication.run(InstitutionManagerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/institution_manager/institution_manager_app/InstitutionManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institution_manager.institution_manager_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutionManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
