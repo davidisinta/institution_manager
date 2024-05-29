49a3c8f8c14d143f96080b6598fc8c3c9b8eb5ff
David Isinta <david.isinta@gmail.com>
2024-05-29 16:22:04 +0300

    added guiding comments and some boilerplate

--- src/main/java/com/institution_manager/institution_manager_app/controllers/CourseController.java
+++ src/main/java/com/institution_manager/institution_manager_app/controllers/CourseController.java
@@ -48,4 +48,38 @@ public class CourseController
         return repo.getAllCourses();
     }
 
+
+    @PostMapping("/addCourse/institution/{id}")
+    public void addCourseToInstitution(@RequestBody Course course)
+    {
+        // take in id of institution and name of course,
+
+
+        //check if coursename Already exists in that institution
+
+
+        //if not, go ahead and add the course to the institution
+    }
+
+    @GetMapping("/courses/institutions/{id}")
+    public Optional<List<Course>> getCoursesByInstitution()
+    {
+
+    }
+
+
+    // filter courses by searching. Be able to change the institution and have the list
+    // of courses change to reflect this
+
+
+    // sort the courses by name in ascending and descending order
+
+
+    // Delete a course - do not delete a course that has been assigned to atleast one student
+    // show relevant errors if user tries to delete a course that has students assigned
+
+
+    // edit name of a course - do not edit a course that has been assigned to atleast one student
+    //    // show relevant errors if user tries to delete a course that has students assigned
+
 }
