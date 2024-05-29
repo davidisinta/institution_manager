049e3aa9c00547195360f86effc011093f628ca2
David Isinta <david.isinta@gmail.com>
2024-05-29 15:16:39 +0300

    boilerplate model for Course

--- src/main/java/com/institution_manager/institution_manager_app/jdbc/Course.java
+++ src/main/java/com/institution_manager/institution_manager_app/jdbc/Course.java
@@ -1,4 +1,28 @@
 package com.institution_manager.institution_manager_app.jdbc;
 
-public class Course {
+public class Course
+{
+    private int courseId;
+    private String courseName;
+
+    public Course(int courseId, String courseName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
 }
